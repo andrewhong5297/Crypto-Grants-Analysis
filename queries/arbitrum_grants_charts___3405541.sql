3b94a62e25a0f7685b876205e0b8221ce53e03ac
Andrew <47720952+andrewhong5297@users.noreply.github.com>
2024-02-05 16:03:38 -0500

    updated queries

--- queries/arbitrum_grants_charts___3405541.sql
+++ queries/arbitrum_grants_charts___3405541.sql
@@ -15,25 +15,33 @@ with
         LEFT JOIN evms.contracts c ON g.grant_blockchain = c.blockchain and contains(split(g.dune_namespaces,','),c.namespace)
     )
     
-    , summary as (
+    , ordered_traces as (
         SELECT 
-            date_trunc('week',tr.block_time) as week
-            , gc.grantee
-            , gc.grant_amount
-            , approx_distinct(tr.tx_hash) as txs
-            , approx_distinct(tx."from") as users
-            , sum(tr.gas_used*tx.gas_price/1e18) as gas_fees_eth
-        FROM grant_contracts gc
-        LEFT JOIN arbitrum.traces tr ON tr.to = gc.address AND gc.grant_blockchain = 'arbitrum'
-        LEFT JOIN arbitrum.transactions tx ON tx.hash = tr.tx_hash
-        WHERE latest = 1
-        AND tr.block_time >= timestamp '2024-01-01 00:00:00'
-        AND tx.block_time >= timestamp '2024-01-01 00:00:00'
-        GROUP BY 1,2,3
+            gc.grantee
+            , gc.grant_blockchain
+            , tr.tx_hash
+            , tr.gas_used
+            , tr.trace_address
+            , tr.block_time
+            , tx."from" as tx_from
+            , tx.gas_price
+            --we only want to keep the highest order trace call by a grantee contract. We do want to keep potential multi-calls so we use rank instead of row_number().
+            , rank() over (partition by gc.grantee, tr.tx_hash order by cardinality(trace_address) asc) as trace_order
+        FROM arbitrum.traces tr 
+        JOIN grant_contracts gc ON tr.to = gc.address
+        JOIN arbitrum.transactions tx ON tx.hash = tr.tx_hash
+        WHERE tr.block_time >= now() - interval '30' day
+        AND tx.block_time >= now() - interval '30' day
+        AND gc.latest = 1
     )
     
 SELECT 
-*
-, grant_amount/users as user_grant_ratio
-FROM summary
+    date_trunc('week',block_time) as week
+    , grantee
+    , approx_distinct(tx_hash) as txs
+    , approx_distinct(tx_from) as users
+    , sum(gas_used*gas_price/1e18) as gas_fees_eth
+FROM ordered_traces
+WHERE trace_order = 1
+GROUP BY 1,2
 ORDER BY week DESC, txs DESC
